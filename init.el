dc1fde311794938f1c977761e89ec61a526fb818
epwr <ericpower@outlook.com>
2021-11-25 20:07:45 -0800

    first commit

---
+++ init.el
@@ -0,0 +1,8 @@
+;; Author: Eric Power
+;;
+;; I've been vaccilating about learning emacs for a while, but what finally put me over was Clojure. I really want to like Clojure: Rich Hickey's design philosophy speaks to me,
+;; I like writing functional code, and having immutable datastructures is wonderful. But Clojure runs on the JVM, which means that running scripts has a startup cost, and this
+;; my typical programming workflow (which involves running what I'm writing frequently). So I was looking at what the Clojure community does to get around this, and it's pretty
+;; clear that a proper REPL for Clojure is viewed as not only a necessity, but one of the best parts of Clojure. And emacs seems to be the gold standard for Clojure REPLs.
+
+;; This directory will start out with a lot 
