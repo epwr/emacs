30dbc497d31a695f24d7cb1ae3afe9ff5f2b5f53
Eric Power <ericpower@outlook.com>
2021-12-12 12:09:16 -0800

    better UI

--- init.el
+++ init.el
@@ -18,16 +18,23 @@
 (dolist (p clojure-packages)
   (unless (package-installed-p p)
     (package-install p)))
-(custom-set-variables
 
-
-
+;; UI Setup
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode) 
+    ;; Load Zenburn Theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'zenburn t)
 
 
 
 
 ;; ---------------------------------------------------------------------------------------------------------
- 
+
+(custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
