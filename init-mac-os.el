dd7ab3975faafa8a67a278d9924235fe8934df72
Eric Power <ericpower@ericpower.com>
2022-11-11 22:36:06 -0800

    Updates, including a new custom theme! Planning on moving the custom theme into it's own repo.

--- init-mac-os.el
+++ init-mac-os.el
@@ -8,10 +8,10 @@
 (global-set-key (kbd "s-<left>") 'move-beginning-of-line)
 
 ;; Add Homebrew to the PATH
-(add-to-list 'load-path "/opt/homebrew/bin")
+(setenv "PATH" (concat (getenv "PATH") ":/opt/homebrew/bin"))
 
 ;; Approprate size for mac  monitor
-(setq initial-frame-alist '((top . 0) (left . 0) (width . 105) (height . 65)))
+(setq initial-frame-alist '((top . 0) (left . 0) (width . 210) (height . 65)))
 
 ;; Point emacs to the proper lein command
 ;; -- run to open the configuration menu.
